686525cc708b43e5946119fd888a852d1b804ab2
tarek-harba <tarek.harba01@gmail.com>
2023-07-09 19:20:01 +0300

    arabic option added

--- snippet.py
+++ snippet.py
@@ -1,5 +1,6 @@
 import fitz
 import sys
+from bidi import algorithm
 
 doc = fitz.open('مسار_ملف_البي_دي_اف')
 
@@ -7,8 +8,12 @@ for page in doc:
 
     text = page.get_text('text')
 
-    with open(f'اسم_الملف_الناتح', 'a', encoding='utf-8') as f:
+    with open(f'اسم_الملف_الناتح.txt', 'a', encoding='utf-8') as f:
 
         sys.stdout = f
+
+        # للفة العربية ابقي على السطر 18
+        # أما إذا كان الملف باللغة الانكليزية فامسح السطر 18
+        text = algorithm.get_display(text)
 
         print(text)
